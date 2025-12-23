24543737b0dd886d07ceff0346a49cafa6069817
Shivam Kumar <shivam2593479@gmail.com>
2025-12-23 19:06:22 +0530

    Configure backend for Vercel

---
+++ api/index.js
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+import connectDB from "../src/config/db.js";
+import app from "../src/app.js";
+
+dotenv.config();
+connectDB();
+
+export default app;
--- src/app.js
+++ src/app.js
@@ -9,21 +9,18 @@ import adminContactRoutes from "./routes/adminContact.routes.js";
 
 const app = express();
 
-// Middlewares
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
-// Health check
 app.get("/", (req, res) => {
   res.send("Rozgarwala API is running..");
 });
 
-// Public Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/profile", profileRoutes);
 app.use("/api/contact", contactRoutes);
-
-// Admin Routes
 app.use("/api/admin/auth", adminAuthRoutes);
 app.use("/api/admin", adminContactRoutes);
 
